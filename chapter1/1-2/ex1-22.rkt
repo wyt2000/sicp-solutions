5b87454e54b75391ef75eb3e1cc3112514a25d3d
wyt2000 <wytwyt200@gmail.com>
2022-07-26 03:05:14 +0000

    [1.2] Add ex1.22 ~ ex1.23

---
+++ chapter1/1-2/ex1-22.rkt
@@ -0,0 +1,42 @@
+#lang sicp
+
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (* test-divisor test-divisor) n) n)
+        ((divides? test-divisor n) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b)
+  (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
+(define (timed-prime-test n)
+  (newline)
+  (display n)
+  (start-prime-test n (runtime)))
+
+(define (start-prime-test n start-time)
+  (if (prime? n)
+      (report-prime (- (runtime) start-time))))
+
+(define (report-prime elapsed-time)
+  (display " *** ")
+  (display elapsed-time)
+  )
+
+(define (search-for-primes from to)
+
+  (define (search-for-primes-iter)
+    (if (= (remainder from 2) 1) (timed-prime-test from))
+    (search-for-primes (+ from 1) to))
+
+  (if (< from to)
+    (search-for-primes-iter)))
+
+(search-for-primes 1000 10000)
+(search-for-primes 10000 100000)
+(search-for-primes 100000 1000000)
+
+;Only when n is big enough, the time cost will be O(log n).
