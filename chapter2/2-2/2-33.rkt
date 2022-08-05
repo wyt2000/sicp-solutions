f451d15205081e77572ac5da15e43f3f241b921d
wyt2000 <wytwyt200@gmail.com>
2022-08-05 11:23:30 +0000

    [2-2] Add ex2-33.

--- chapter2/2-2/2-33.rkt
+++ chapter2/2-2/2-33.rkt
@@ -4,16 +4,20 @@
   (define (iter seq result)
           (if (null? seq)
 	      result
-	      (iter (cdr seq) (p result (car seq)))))
+	      (iter (cdr seq) (p (car seq) result))))
   (iter seq zero))
 
+(define (append seq1 seq2)
+  (accumulate cons seq2 (reverse seq1)))
+
+(append (list 1 2 3) (list 4 5 6))
+
 (define (map p sequence)
-  (accumulate (lambda (x y) (cons (p x) y)) nil sequence))
+  (accumulate (lambda (x y) (append y (list (p x)))) nil sequence))
 
 (map (lambda (x) (+ x 1)) (list 1 2 3 4 5))
 
-;(define (append seq1 seq2)
-;  (accumulate cons <??> <??>))
-;
-;(define (length sequence)
-;  (accumulate <??> 0 sequence))
+(define (length sequence)
+  (accumulate (lambda (x y) (+ y 1)) 0 sequence))
+
+(length (list 1 2 3 4 5))
